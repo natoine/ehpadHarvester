7ba3d17b40eaa832d1d8048b7fd1ce2b4f002ecd
natoine <antoinseilles@gmail.com>
2018-10-31 20:11:43 +0100

    now pass url through a form

--- application/routes/mainroutes.js
+++ application/routes/mainroutes.js
@@ -9,27 +9,28 @@ module.exports = function(app, express) {
 
     mainRoutes.get('/', function(req, res){
     	res.render('index')
+
+    	//get the ?url= query tagid from request
+        reqURL = req.query.url
+
+        if(reqURL != null)
+        {
+        	console.log("url : " + reqURL)
+        	request(reqURL, function(error, response, html){
+
+		  		//console.log('body:', html);
+		  		console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+
+				if(!error)
+				{
+
+				}
+				else console.log('error:', error)
+			})
+
+        }
+        else console.log("url is null :(")
     })
-
-	mainRoutes.get('/scrape', function(req, res){
-
-		console.log("gonna scrape")
-
-		url = 'https://www.pour-les-personnes-agees.gouv.fr/annuaire-ehpad-en-hebergement-permanent/13/0'
-
-		request(url, function(error, response, html){
-
-	  		console.log('body:', html);
-	  		console.log('error:', error); // Print the error if one occurred
-	  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-			if(!error)
-			{
-
-			}
-		})
-	})
-
 
 	// apply the routes to our application
     app.use('/', mainRoutes)
