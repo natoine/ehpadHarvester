102278c6f75f80de7e4b0e77731a8b21da6c8f81
natoine <antoinseilles@gmail.com>
2018-10-31 18:20:16 +0100

    now get some routes

---
+++ application/routes/mainroutes.js
@@ -0,0 +1,37 @@
+
+const request = require('request')
+
+// application/routes.js
+module.exports = function(app, express) {
+
+    // get an instance of the router for main routes
+    const mainRoutes = express.Router()
+
+    mainRoutes.get('/', function(req, res){
+    	res.send('Hello World!')
+    })
+
+	mainRoutes.get('/scrape', function(req, res){
+
+		console.log("gonna scrape")
+
+		url = 'https://www.pour-les-personnes-agees.gouv.fr/annuaire-ehpad-en-hebergement-permanent/13/0'
+
+		request(url, function(error, response, html){
+
+	  		console.log('body:', html);
+	  		console.log('error:', error); // Print the error if one occurred
+	  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+			if(!error)
+			{
+
+			}
+		})
+	})
+
+
+	// apply the routes to our application
+    app.use('/', mainRoutes)
+
+}
--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-var express = require('express')
-var app = express()
+const port     = process.env.PORT || 8080
+
+const express = require('express')
+const app = express()
 
 const morgan       = require('morgan')
 app.use(morgan('dev')) // log every request to the console
 
-var request = require('request')
-
 app.set('view engine', 'ejs') // set up ejs for templating
 
-app.get('/scrape', function(req, res){
+// routes ======================================================================
+require('./application/routes/mainroutes.js')(app, express)
 
-	console.log("gonna scrape")
-
-	url = 'https://www.pour-les-personnes-agees.gouv.fr/annuaire-ehpad-en-hebergement-permanent/13/0'
-
-	request(url, function(error, response, html){
-
-  		console.log('body:', html);
-  		console.log('error:', error); // Print the error if one occurred
-  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-
-		if(!error)
-		{
-
-		}
-	})
-
-
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// launch ======================================================================
+app.listen(port)
+console.log('The magic happens on port ' + port)
