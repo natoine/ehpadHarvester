f4a8517c3df15d39bd612326a22d382c3925ce9d
natoine <antoinseilles@gmail.com>
2018-11-01 16:50:53 +0100

    no print in view error and statuscode

--- application/routes/mainroutes.js
+++ application/routes/mainroutes.js
@@ -8,7 +8,6 @@ module.exports = function(app, express) {
     const mainRoutes = express.Router()
 
     mainRoutes.get('/', function(req, res){
-    	res.render('index')
 
     	//get the ?url= query tagid from request
         reqURL = req.query.url
@@ -21,17 +20,28 @@ module.exports = function(app, express) {
         	request(reqURL, function(error, response, html){
 
 		  		//console.log('body:', html);
-		  		console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+		  		statuscode = response && response.statusCode
+		  		console.log('statuscode: ', statuscode) // Print the response status code if a response was received
 
 				if(!error)
 				{
-
+					res.render('index', {statuscode: statuscode ,
+											error: "no error"})
 				}
-				else console.log('error:', error)
+				else 
+				{
+					console.log('error: ', error)
+					res.render('index', {statuscode: 0 , 
+											error: error})
+				}
 			})
 
         }
-        else console.log("url is null :(")
+        else
+        {
+        	res.render('index', {statuscode: 0 ,
+        							error: "no url specified"})
+        } 
     })
 
 	// apply the routes to our application
