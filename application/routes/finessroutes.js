fc9346488d82f578b0751b7d060ef2719df11608
natoine <antoineseilles@gmail.com>
2019-02-22 14:01:26 +0100

    seems to extract only the interesting lines

--- application/routes/finessroutes.js
+++ application/routes/finessroutes.js
@@ -3,8 +3,12 @@
 const fs = require('fs')
 const path = require('path')
 const readline = require('readline')
-const stream = require('stream')
 const parse = require('csv-parse')
+
+const categories = [ '1101', '1102', '1106', '1107', '1109', '1110', '1201', '4101', '4103', '4105', '4106',
+                        '4107', '4301', '4304', '4305', '4401', '4402', '4404' ]
+
+const subcategories = [ '270' ]
 
 // application/routes.js
 module.exports = function(app, express) {
@@ -27,7 +31,8 @@ module.exports = function(app, express) {
                 if(err) console.error("problem parsing line ", line, err)
                 else 
                 {
-                    if(parsedline[0][20] && parsedline[0][20].trim() === "4404") output.push(parsedline[0])
+                    if(parsedline[0][20] && categories.includes(parsedline[0][20].trim())) output.push(parsedline[0])
+                    else if(parsedline[0][18] && subcategories.includes(parsedline[0][18].trim())) output.push(parsedline[0])
                 }
             })
         })
