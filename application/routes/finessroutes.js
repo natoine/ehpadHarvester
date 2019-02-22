be8e65055559f42d2fbd70dd9c2e5079c99358c7
natoine <antoineseilles@gmail.com>
2019-02-22 13:50:24 +0100

    now get the 4404 etabs

--- application/routes/finessroutes.js
+++ application/routes/finessroutes.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const readline = require('readline')
 const stream = require('stream')
+const parse = require('csv-parse')
 
 // application/routes.js
 module.exports = function(app, express) {
@@ -13,21 +14,28 @@ module.exports = function(app, express) {
 
     finessRoutes.get("/interestingetabs", function(req, res) {
         var instream = fs.createReadStream(path.join("ressources", "listeFinessJanvier2019.csv"))
-        var outstream = new stream
-        outstream.readable = true
-        outstream.writable = true
 
         var rl = readline.createInterface({
             input: instream,
-            output: outstream,
             terminal: false
         })
 
+        var output = []
+
         rl.on('line', function(line) {
-            console.log(line)
-            //Do your stuff ...
-            //Then write to outstream
+            parse(line, { delimiter: ';' }, function(err, parsedline){
+                if(err) console.error("problem parsing line ", line, err)
+                else 
+                {
+                    if(parsedline[0][20] && parsedline[0][20].trim() === "4404") output.push(parsedline[0])
+                }
+            })
         })
+
+        rl.on('close', function() {
+            console.log("end", output)
+        })
+
     })
 
     // apply the routes to our application
